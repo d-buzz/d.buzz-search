a190ffefa98242ebd394bb60d7b5ee13c3c7471a
stinkymonkeyph <monkeydev.team@gmail.com>
2020-08-16 02:24:47 +0800

    round robin api key for testing

--- src/api/controllers/search.js
+++ src/api/controllers/search.js
@@ -4,19 +4,30 @@ const searchSort = { sort: "popularity" }
 const searchApiKey = 'K1GJEM2XVI4LISLEZAYREFH7JDOCFMDFIDSHN62EUHD6SLASLDHFDEQ25SRV'
 const fetch = require('node-fetch')
 
-const baseRequest = {
+const apiKeys = [
+  'K1GJEM2XVI4LISLEZAYREFH7JDOCFMDFIDSHN62EUHD6SLASLDHFDEQ25SRV',
+  'PHTO8LRTIH7HECN0WRTJYOH2P584UDI0G1CE0QAEV4YRFZOZJVFIXQD46AFJ'
+]
+
+let baseRequest = {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json',
-    'Authorization': searchApiKey,
+    'Authorization': '',
     'mode': 'no-cors',
     'cache': 'no-cache',
   },
 }
 
+const randomIndex = (minimum, maximum) => {
+  return Math.round( Math.random() * (maximum - minimum) + minimum);
+}
+
 const searchPostByTags = async (req, res) => {
   const tag = req.body.tag
   const body = { q: `${tag} tag:${tag} ${categoryParam}`, ...searchSort }
+  const apiKey = apiKeys[randomIndex(0, 1)]
+  baseRequest = baseRequest.headers.Authorization = apiKey
   const request = {
     ...baseRequest,
     body: JSON.stringify(body),
@@ -31,6 +42,8 @@ const searchPostByTags = async (req, res) => {
 const searchPostByQueryString = async (req, res) => {
   const tag = req.body.query
   const body = { q: `${tag} ${categoryParam}`, ...searchSort }
+  const apiKey = apiKeys[randomIndex(0, 1)]
+  baseRequest = baseRequest.headers.Authorization = apiKey
   const request = {
     ...baseRequest,
     body: JSON.stringify(body),
