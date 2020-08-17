8ee81f8a1d8a0a309cf67c960389bc1bcd5c7db1
stinkymonkeyph <monkeydev.team@gmail.com>
2020-08-18 03:20:36 +0800

    register search by author in routes

--- src/api/routes/searchRouter.js
+++ src/api/routes/searchRouter.js
@@ -1,9 +1,14 @@
 const { Router } = require('express')
-const { searchPostByTags, searchPostByQueryString } = require('../controllers/search')
+const { 
+  searchPostByTags,
+  searchPostByQueryString,
+  searchPostByAuthor,
+} = require('../controllers/search')
 
 const searchRouter = Router()
 
 searchRouter.post('/tags', searchPostByTags)
 searchRouter.post('/query', searchPostByQueryString)
+searchRouter.post('/author', searchPostByAuthor)
 
 module.exports = searchRouter
